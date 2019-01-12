436f7f8510bbbddbbb1c2da00e5d3cab1afb534b
hoanghongchuong <chuonghoanghong@gmail.com>
2019-01-12 09:34:59 +0700

    a

--- index.php
+++ index.php
@@ -350,7 +350,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/DGixjx5vpiM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="100%" height="415" src="https://www.youtube.com/embed/DGixjx5vpiM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
